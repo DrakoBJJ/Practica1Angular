44402355b4925168d45b8c170c8171e56bdb0768
Zdravko Plamenov Nikolov <primico8bull@gmail.com>
2024-11-21 17:00:51 +0100

    cambiar url
    cambio de las urls para las imagenes

--- src/app/componentes/imagenes/imagenes.component.ts
+++ src/app/componentes/imagenes/imagenes.component.ts
@@ -30,23 +30,23 @@ export class ImagenesComponent implements OnInit {
 
   //lista de ciudades y Url de las imagenes 
   ciudades: Ciudad[] = [
-    {nombreCiudad: 'Madrid', imagenUrl: 'https://via.placeholder.com/350x200.png?text=Madrid'},
-    {nombreCiudad: 'Barcelona', imagenUrl: 'https://via.placeholder.com/350x200.png?text=Barcelona'},
-    {nombreCiudad: 'Alicante', imagenUrl: 'https://via.placeholder.com/350x200.png?text=Alicante'},
-    {nombreCiudad: 'Sevilla', imagenUrl: 'https://via.placeholder.com/350x200.png?text=Sevilla'},
-    {nombreCiudad: 'Valencia', imagenUrl: 'https://via.placeholder.com/350x200.png?text=Valencia'},
-    {nombreCiudad: 'Granada', imagenUrl: 'https://via.placeholder.com/350x200.png?text=Granada'},
-    {nombreCiudad: 'Bilbao', imagenUrl: 'https://via.placeholder.com/350x200.png?text=Bilbao'},
-    {nombreCiudad: 'Zaragoza', imagenUrl: 'https://via.placeholder.com/350x200.png?text=Zaragoza'},
-    {nombreCiudad: 'Malaga', imagenUrl: 'https://via.placeholder.com/350x200.png?text=Malaga'},
-    {nombreCiudad: 'Salamanca', imagenUrl: 'https://via.placeholder.com/350x200.png?text=Salamanca'},
-    {nombreCiudad: 'Pamplona', imagenUrl: 'https://via.placeholder.com/350x200.png?text=Pamplona'},
-    {nombreCiudad: 'Murcia', imagenUrl: 'https://via.placeholder.com/350x200.png?text=Murcia'},
-    {nombreCiudad: 'Santander', imagenUrl: 'https://via.placeholder.com/350x200.png?text=Santander'},
-    {nombreCiudad: 'Burgos', imagenUrl: 'https://via.placeholder.com/350x200.png?text=Burgos'},
-    {nombreCiudad: 'Cádiz', imagenUrl: 'https://via.placeholder.com/350x200.png?text=Cádiz'},
-    {nombreCiudad: 'Palma', imagenUrl: 'https://via.placeholder.com/350x200.png?text=Palma'},
-    {nombreCiudad: 'A Coruña', imagenUrl: 'https://via.placeholder.com/350x200.png?text=A+Coruña'}
+    {nombreCiudad: 'Madrid', imagenUrl: 'https://a.cdn-hotels.com/gdcs/production133/d1207/7ad2d7f0-68ce-11e8-8a0f-0242ac11000c.jpg'},
+    {nombreCiudad: 'Barcelona', imagenUrl: 'https://europamundoblog.com/wp-content/uploads/2019/01/sagrada-familia.jpg'},
+    {nombreCiudad: 'Alicante', imagenUrl: 'https://static.ledbox.es/posicionamiento/provincias/alicante.jpg'},
+    {nombreCiudad: 'Sevilla', imagenUrl: 'https://th.bing.com/th/id/R.e182243a28e7f7b333ca0841f49df311?rik=SYOXe6m0WkMImg&pid=ImgRaw&r=0'},
+    {nombreCiudad: 'Valencia', imagenUrl: 'https://th.bing.com/th/id/OIP.-59cOEPwWTxuvbQHCow36wHaE8?rs=1&pid=ImgDetMain'},
+    {nombreCiudad: 'Granada', imagenUrl: 'https://elviajerofeliz.com/wp-content/uploads/2018/04/Que-ver-en-Granada.jpg'},
+    {nombreCiudad: 'Bilbao', imagenUrl: 'https://th.bing.com/th/id/R.b695ef5f33e845fd7a6d6763c9124c54?rik=6CwanCNCF%2ffVTA&pid=ImgRaw&r=0'},
+    {nombreCiudad: 'Zaragoza', imagenUrl: 'https://th.bing.com/th/id/OIP.q0GSdFoIYOou4Pp-IcRgZQHaEb?rs=1&pid=ImgDetMain'},
+    {nombreCiudad: 'Malaga', imagenUrl: 'https://th.bing.com/th/id/OIP.iyWg2EK-o1qDrgkgt0sPwwHaEl?rs=1&pid=ImgDetMain'},
+    {nombreCiudad: 'Salamanca', imagenUrl: 'https://oleganatravelboutique.com/wp-content/uploads/2016/02/Salamanca_Cathedral_-_after_sunrise-2.jpg'},
+    {nombreCiudad: 'Pamplona', imagenUrl: 'https://th.bing.com/th/id/OIP.5EYvNRSO08EJELYg8IqZBQHaE7?rs=1&pid=ImgDetMain'},
+    {nombreCiudad: 'Murcia', imagenUrl: 'https://th.bing.com/th/id/OIP.EVbzBoMynrV9WWgz1Eu4fQHaFU?rs=1&pid=ImgDetMain'},
+    {nombreCiudad: 'Santander', imagenUrl: 'https://th.bing.com/th/id/R.ec6be32f85969a76d473ceaad62fd777?rik=wrW7Tay4cZLiFQ&riu=http%3a%2f%2fevasion-online.com%2fimagearticle%2f2016%2f05%2fsantander-espagne.jpg&ehk=gchInJSb4%2fofj%2fq0%2bHb%2f5%2fme%2fVeVPD3KAfb5kb9aIUU%3d&risl=&pid=ImgRaw&r=0'},
+    {nombreCiudad: 'Burgos', imagenUrl: 'https://th.bing.com/th/id/R.bfb9442fb5693b3a1e66888927bd2883?rik=uNDHT5Khac7jyQ&riu=http%3a%2f%2faunclicdelaaventura.com%2fwp-content%2fuploads%2f2015%2f01%2fBurgos-Nevado-031-Modificada.jpg&ehk=imP94kKy2OlW0hbXJLREMoZ5ybwrq9w6icL4EMx%2bZgA%3d&risl=&pid=ImgRaw&r=0'},
+    {nombreCiudad: 'Cádiz', imagenUrl: 'https://www.cadizturismo.com/storage/app/media/uploaded-files/p-cadiz_turismo.jpg'},
+    {nombreCiudad: 'Palma', imagenUrl: 'https://lonelyplanetimages.imgix.net/mastheads/GettyImages-535651867_super.jpg'},
+    {nombreCiudad: 'A Coruña', imagenUrl: 'https://turismo20.com/wp-content/uploads/2020/01/La-Coruna.jpg'}
     
   ];
   
